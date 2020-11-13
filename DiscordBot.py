4f446be5e4b1aea4dd2c28052532d96eb234c924
OHvrything <66000432+OHvrything@users.noreply.github.com>
2020-11-13 15:08:36 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -57,46 +57,6 @@ async def ping(ctx):
 async def ping(ctx):
     await ctx.send('깃헙 띵이봇 위키를 참고하세요!')
     await ctx.send('> 띵이봇 위키: https://github.com/OHvrything/thingebotV2/wiki')
-    
-@bot.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-        
-        await update_data(users, member)
-        
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-    
-@bot.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-        
-        await update_data(users, message.author)
-        await add_experience(users, message.author, 5)
-        await level_up(users, message.author, message.channel)
-        
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-        
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 1
-        
-async def add_experience(users, user, exp):
-    users[user.id]['experience'] += experience
-    
-async def level_up(users, user, channel):
-    experience = users[user.id]['experience']
-    lvl_start = users[user.id]['level']
-    lvl.end = int(experience ** (1/4))
-    
-    if lvl_start < lvl_end:
-        await bot.send_message(channel, '{}님께서 레벨 {}로 레벨업 하셨습니다!'.format(user.mention, lvl_end))
-        users[user.id]['level'] = lvl_end
         
 
 bot.run(os.environ['token'])
